e9058017c6f856dc1e524437f689d5f9a5619c11
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2022-07-28 15:12:13 +0300

    fixing scss and responsive

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document.addEventListener("click", (e) => {
   if (e.target.matches(".expand-button1")) {
     const card = e.target.closest(".show-card")
-    const cardBody = card.querySelector(".business")
+    const cardBody = card.querySelector(".contact__business")
 
     cardBody.classList.remove("show")
   }
@@ -12,7 +12,7 @@ document.addEventListener("click", (e) => {
 document.addEventListener("click", (e) => {
   if (e.target.matches(".expand-button")) {
     const card = e.target.closest(".show-card")
-    const cardBody = card.querySelector(".business")
+    const cardBody = card.querySelector(".contact__business")
 
     cardBody.classList.add("show")
   }
