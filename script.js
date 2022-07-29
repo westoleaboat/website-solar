9c5bc4eb62eff41b48beaa7ce183494d83292867
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2022-07-29 20:25:31 +0300

    improved html layout

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-// console.log("hi")
+console.log("hi")
 
 document.addEventListener("click", (e) => {
-  if (e.target.matches(".expand-button1")) {
-    const card = e.target.closest(".show-card")
-    const cardBody = card.querySelector(".contact__business")
+  if (e.target.matches(".contact__text__buttons__expand-button__business")) {
+    const form = e.target.closest(".contact__text")
+    const formPrivate = form.querySelector(".contact__text__private-form")
+    const formBusiness = form.querySelector(".contact__text__business-form")
 
-    cardBody.classList.remove("show")
+    formBusiness.classList.add("show")
+    formPrivate.classList.remove("show")
+
+    // let btnBusiness = document.querySelector(
+    //   ".contact__text__expand-button__business"
+    // )
+    // btnBusiness.classList.add("active")
   }
 })
 
 document.addEventListener("click", (e) => {
-  if (e.target.matches(".expand-button")) {
-    const card = e.target.closest(".show-card")
-    const cardBody = card.querySelector(".contact__business")
+  if (e.target.matches(".contact__text__buttons__expand-button__private")) {
+    const form = e.target.closest(".contact__text")
+    const formPrivate = form.querySelector(".contact__text__private-form")
+    const formBusiness = form.querySelector(".contact__text__business-form")
 
-    cardBody.classList.add("show")
+    formBusiness.classList.remove("show")
+    formPrivate.classList.add("show")
+
+    // let btnBusiness = document.querySelector(
+    //   ".contact__text__expand-button__business"
+    // )
+    // btnBusiness.classList.add("active")
   }
 })
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.matches(".contact__text__buttons__expand-button__private")) {
+//     const form = e.target.closest(".contact__text")
+//     const formPrivate = form.querySelector(".contact__text__private-form")
+
+//     formPrivate.classList.toggle("show")
+
+//     // let btnBusiness = document.querySelector(
+//     //   ".contact__text__expand-button__business"
+//     // )
+//     // btnBusiness.classList.add("active")
+//   }
+// })
+
+// const form = e.target.closest(".contact__text")
+// const formPrivate = form.querySelector(".contact__text__private-form")
+
+let functiOn = () => {
+  const form = document.querySelector(".contact__text")
+  const formPrivate = form.querySelector(".contact__text__private-form")
+  formPrivate.classList.toggle("show")
+}
+
+functiOn()
